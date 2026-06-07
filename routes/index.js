16cb9dca2ebb61a104fbddc1091f0fefad72221b
Dhaya-max <dhayanithimanivannan@gmail.com>
2026-06-07 11:00:49 +0530

    allow all cors

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,5 @@
-const http = require('http')
-const { Server } = require('socket.io')
-const { connectDB } = require('./models/db')
-const handleRequest = require('./routes/index')
-require('dotenv').config()
-
-const PORT = process.env.PORT || 5000
-
 const server = http.createServer((req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://pec-bus-tracker-s8ru.vercel.app')
+  res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
 
@@ -21,19 +13,5 @@ const server = http.createServer((req, res) => {
 })
 
 const io = new Server(server, {
-  cors: { origin: 'https://pec-bus-tracker-s8ru.vercel.app', methods: ['GET', 'POST'] }
-})
-
-io.on('connection', (socket) => {
-  console.log('Client connected:', socket.id)
-  socket.on('driver:update', (data) => {
-    io.emit('bus:updated', data)
-  })
-  socket.on('disconnect', () => {
-    console.log('Client disconnected:', socket.id)
-  })
-})
-
-connectDB().then(() => {
-  server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+  cors: { origin: '*', methods: ['GET', 'POST'] }
 })
